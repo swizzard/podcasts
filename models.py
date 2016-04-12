84b9e6d232a35ef4b10c424a186174e64684be17
Sam Raker <sam.raker@gmail.com>
2016-04-12 16:35:56 -0400

    remove obsolete relationships

--- models.py
+++ models.py
@@ -48,9 +48,6 @@ class Podcast(Base):
     homepage = schema.Column(types.String(255))
     summary = schema.Column(types.Text(collation='utf8_general_ci')) 
 
-    week_days = orm.relationship('WeekDay', secondary='week_days_to_podcasts')
-    days_of_month = orm.relationship('DayOfMonth',
-                                     secondary='days_of_month_to_podcasts')
     categories = orm.relationship('Category', secondary='categories_to_podcasts')
     episodes = orm.relationship('Episode', backref='podcast')
 
